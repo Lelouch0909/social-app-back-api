2f4a3655a3a313c5c58031688ae97587207534ae
Lelouch0909 <lnhm87404@gmail.com>
2024-01-18 23:02:09 +0100

    new file:   .gitignore 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/com/lontsi/devsocialmedia/DevsocialmediaApplication.java 	new file:   src/main/java/com/lontsi/devsocialmedia/config/AppConfig.java 	new file:   src/main/java/com/lontsi/devsocialmedia/config/JwtConstant.java 	new file:   src/main/java/com/lontsi/devsocialmedia/config/JwtTokenValidator.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/lontsi/devsocialmedia/DevsocialmediaApplicationTests.java

---
+++ src/main/java/com/lontsi/devsocialmedia/DevsocialmediaApplication.java
@@ -0,0 +1,14 @@
+package com.lontsi.devsocialmedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevsocialmediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevsocialmediaApplication.class, args);
+
+	}
+
+}
---
+++ src/main/java/com/lontsi/devsocialmedia/config/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.lontsi.devsocialmedia.config;
+
+public class JwtConstant {
+
+	public static final String SECRET_KEY = "dvniuSDNiuDKonOnnnOIDossodoicjsdjcbisudonqdocnsbudisud";
+	
+	public static final String JWT_HEADER = "Authorization";
+}
